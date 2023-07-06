8c9b891e48262be58be20bc32370aa4bf881357e
Ryans <ryangodwin@uabmc.edu>
2023-07-06 15:39:52 -0500

    testing

--- cc-codebase/hooks/post_gen_project.sh
+++ cc-codebase/hooks/post_gen_project.sh
@@ -17,4 +17,4 @@ git push origin HEAD
 
 
 
-cookiecutter https://github.com/UABPeriopAI/MLOpsTemplate --checkout rcg-uab-patch-1 --directory="cc-data" --no-input project_name="{{cookiecutter.project_name}}" data_dir="{{cookiecutter.data_dir}}" data_directory_name="DATASCI"
+cookiecutter https://github.com/UABPeriopAI/MLOpsTemplate --checkout rcg-uab-patch-1 -o {{cookiecutter.data_dir}} --directory="cc-data" --no-input project_name="{{cookiecutter.project_name}}" data_dir="{{cookiecutter.data_dir}}" data_directory_name="DATASCI"
