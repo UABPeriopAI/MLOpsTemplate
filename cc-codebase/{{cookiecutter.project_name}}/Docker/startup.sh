be77634e72da745e39a4d919b75ccc5d4a3c26cc
Ryan Godwin <97175225+rcg-uab@users.noreply.github.com>
2023-10-06 13:06:11 -0500

    Clean dev reqs

--- cc-codebase/{{cookiecutter.project_name}}/Docker/startup.sh
+++ cc-codebase/{{cookiecutter.project_name}}/Docker/startup.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
-git flow init -d
-
-#to test the package as you go
-cd src/
 python3 -m pip install pip setuptools wheel
 python3 -m pip install -e ".[dev]"
-cd ../
 
 #startup the MLFlow server
 mlflow server --backend-store-uri sqlite:////data/DATASCI/lab_notebook/mlflow.db --default-artifact-root /data/DATASCI/lab_notebook/mlruns
